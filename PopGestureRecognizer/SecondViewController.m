97e3014da2f3f520b7cf4efee32eec283377a149
景军 <jingjun@jingjundeMacBook-Air.local>
2017-12-24 14:12:58 +0800

    真正的边缘侧滑返回
    解决了全屏侧滑返回容易触发其他手势的缺点，比系统自带侧滑返回更加方便

--- PopGestureRecognizer/SecondViewController.m
+++ PopGestureRecognizer/SecondViewController.m
@@ -23,18 +23,19 @@
 }
 - (void)setScrollView{
     UIScrollView *scroll = [[UIScrollView alloc]initWithFrame:self.view.frame];
-    scroll.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width * 5, self.view.frame.size.height);
+    scroll.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width * 3, self.view.frame.size.height);
     scroll.pagingEnabled = YES;
     NSArray *arr = @[[UIColor greenColor],[UIColor blueColor],[UIColor yellowColor]];
-    
-    for (int i = 0; i < 2; i++) {
+    [self.view addSubview:scroll];
+    for (int i = 0; i < 3; i++) {
         UIView *view = [[UIView alloc]initWithFrame:CGRectMake(i * [UIScreen mainScreen].bounds.size.width, 0, [UIScreen mainScreen].bounds.size.width, self.view.frame.size.height)];
         view.backgroundColor = arr[i];
         [scroll addSubview:view];
         
-        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(30, self.view.frame.size.width / 3, [UIScreen mainScreen].bounds.size.width - 60, 80)];
+        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(30, self.view.frame.size.width / 3, [UIScreen mainScreen].bounds.size.width - 60, 120)];
         label.text = @"第二页view有一个scrollview,在scrollview边缘会触发右划返回手势，在其他地方滑动触发scollview滚动手势，相比全屏侧滑返回手势，减少了其他手势与右划手势的冲突，相对系统自带的侧滑手势更加简单";
         label.font = [UIFont systemFontOfSize:16];
+        label.numberOfLines = 0;
         label.textAlignment = NSTextAlignmentCenter;
         label.textColor = [UIColor whiteColor];
         [self.view addSubview:label];
@@ -49,13 +50,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
+
